1e83de4ff7648eeb46edbbdd353dde9b72d9c4cb
taoyuan <torworx@gmail.com>
2014-06-24 20:36:40 +0800

    haproxy install

--- haproxy-install.js
+++ haproxy-install.js
@@ -3,5 +3,5 @@
 var sh = require('shelljs');
 
 sh.exec('sudo add-apt-repository -y ppa:vbernat/haproxy-1.5');
-sh.exec('sudo apt-get update');
-sh.exec('sudo apt-get install haproxy');
+sh.exec('sudo apt-get -yq update');
+sh.exec('sudo apt-get -yq install haproxy');
