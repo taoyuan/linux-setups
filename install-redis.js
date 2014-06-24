da9b76ba7c1ec9bf89995aaf5ba9985d203aa474
taoyuan <torworx@gmail.com>
2014-06-24 11:42:12 +0800

    install-redis

--- install-redis.js
+++ install-redis.js
@@ -3,4 +3,26 @@
 var sh = require('shelljs');
 var prop = require('properties-parser');
 
-sh.exec('sudo apt-get -yq update', { silent: true });
+var opts = { silent: false },
+    cmds;
+
+if (sh.which('redis-server')) {
+    var version = sh.exec('redis-server', {silent: true}).output;
+    sh.echo('redis v', version, 'has been installed, exit now');
+    sh.exit(1);
+}
+
+// Install redis-server
+cmds = [
+    'sudo apt-get -yq update',
+    'sudo apt-get -yq install python-software-properties',
+    'sudo add-apt-repository -y ppa:rwky/redis',
+    'sudo apt-get -yq update',
+    'sudo apt-get -yq install redis-server'
+];
+
+commands.forEach(function (cmd) {
+    sh.exec(cmd, opts);
+});
+
+// Config redis
